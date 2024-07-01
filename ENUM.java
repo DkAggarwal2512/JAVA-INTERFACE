e4eab18a07ce27216985a7e2efe731ef25d8a554
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-07-01 12:24:38 +0530

    Update ENUM.java

--- ENUM.java
+++ ENUM.java
@@ -1,20 +1,10 @@
 class A{
     int a; int b; int c;
 }
-
-
-
-
-
-
 enum Students{
     Seema,
     DK,
     RK;
-
-
-
-
 
 // this is called methods
 //     public void show(){
@@ -39,12 +29,22 @@ public class ENUM {
         // System.out.println(obj);   
         // obj.show();
 
-       Students obj[] = Students.values();
-        System.out.println(obj);
-        for(int i =0; i<obj.length;i++){
-            System.out.println(obj[i]);
 
 
+        // Students obj[] = Students.values();
+        // System.out.println(obj);
+        // for(int i =0; i<obj.length;i++){
+        //     System.out.println(obj[i]);
+
+
+        // }
+
+
+        for(Students s : Students.values()){
+            // System.out.println(s.ordinal()+1+" "+s);
+            System.out.println(s.name());
+            System.out.println(s.toString());
+            System.out.println(s.valueOf("Seema"));
         }
 
 
