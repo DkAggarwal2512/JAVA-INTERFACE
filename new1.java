5264056e353118df3a2f719af88cf71cc88900dc
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-07-01 11:05:15 +0530

    Add files via upload

---
+++ new1.java
@@ -0,0 +1,12 @@
+public class new1 {
+    public static void main(String[] args){
+        // if we use final keyword then we can't be reassigned the variable & this is called compile time error.
+         final int age = 24;
+        // age=23;
+        final String name = "Seema Aggarwal";
+        // name="DK";
+        
+        System.out.println(" I m " + age + " Years old, & my name is " + name);
+    }
+}
+
